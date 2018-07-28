63d1ced5295af16f85a63736b1b51c5c75f8ce76
SanctaCC <mraxeo@gmail.com>
2018-07-28 16:58:05 +0200

    Initial Spring Boot commit

---
+++ src/main/java/com/sanctaultras/encryptedchat/EncryptedChatApplication.java
@@ -0,0 +1,12 @@
+package com.sanctaultras.encryptedchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptedChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptedChatApplication.class, args);
+	}
+}
