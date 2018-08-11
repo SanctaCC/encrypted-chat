fdf7a7ed37eb195719042603cd6abb78b06a13be
SanctaCC <mraxeo@gmail.com>
2018-08-11 14:28:42 +0200

    cors change

--- src/main/java/com/sanctaultras/encryptedchat/web/WebConfig.java
+++ src/main/java/com/sanctaultras/encryptedchat/web/WebConfig.java
@@ -1,29 +1,18 @@
 package com.sanctaultras.encryptedchat.web;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import java.util.Arrays;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @Slf4j
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer{
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Arrays.asList("*"));
-        corsConfig.setAllowedMethods(Arrays.asList("*"));
-        corsConfig.setAllowedHeaders(Arrays.asList("*"));
-        corsConfig.setAllowCredentials(true);
-        source.registerCorsConfiguration("/**", corsConfig);
-        return new CorsFilter(source);
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowCredentials(true).allowedHeaders("*")
+                .allowedMethods("*").allowedOrigins("*");
+
     }
-
-
 }
