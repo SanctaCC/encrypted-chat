8c1ac4a544933bffcc8bc35635f4d69962477200
SanctaCC <mraxeo@gmail.com>
2018-08-13 22:56:49 +0200

    basic audit, userSet serializer

--- src/main/java/com/sanctaultras/encryptedchat/commons/BaseEntity.java
+++ src/main/java/com/sanctaultras/encryptedchat/commons/BaseEntity.java
@@ -3,6 +3,7 @@ package com.sanctaultras.encryptedchat.commons;
 import lombok.Getter;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Getter
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
@@ -11,4 +12,19 @@ public abstract class BaseEntity<T> {
 
     @Id @GeneratedValue
     private T id;
+
+    private Date createdDate;
+
+    private Date lastModified;
+
+    @PrePersist
+    private void prePersist() {
+        createdDate = new Date();
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        lastModified = new Date();
+    }
+
 }
--- src/main/java/com/sanctaultras/encryptedchat/user/chat/ChatRoom.java
+++ src/main/java/com/sanctaultras/encryptedchat/user/chat/ChatRoom.java
@@ -1,8 +1,9 @@
 package com.sanctaultras.encryptedchat.user.chat;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.JsonIdentityReference;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.sanctaultras.encryptedchat.commons.BaseEntity;
 import com.sanctaultras.encryptedchat.user.User;
 import lombok.AllArgsConstructor;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
+import java.io.IOException;
 import java.util.Set;
 import java.util.UUID;
 
@@ -23,8 +25,30 @@ public class ChatRoom extends BaseEntity<UUID> {
     private String name;
 
     @ManyToMany(mappedBy = "userRooms")
-    @JsonIdentityInfo(generator=ObjectIdGenerators.PropertyGenerator.class, property="id")
-    @JsonIdentityReference(alwaysAsId=true)
+    @JsonSerialize(using=UserSetSerializer.class)
     private Set<User> users;
 
+}
+class UserSetSerializer extends JsonSerializer<Set<User>> {
+
+    @Override
+    public void serialize(Set<User> value, JsonGenerator jsonGenerator, SerializerProvider provider)
+            throws IOException {
+
+        jsonGenerator.writeStartArray();
+        value.forEach( p-> {
+            try {
+                jsonGenerator.writeStartObject();
+                jsonGenerator.writeNumberField("id",p.getId());
+                jsonGenerator.writeStringField("email",p.getEmail());
+                jsonGenerator.writeEndObject();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+        });
+        jsonGenerator.writeEndArray();
+    }
 }
