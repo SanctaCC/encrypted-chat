77500cc67a8b397cf520e1c0cd75657ae459b13e
SanctaCC <mraxeo@gmail.com>
2018-08-23 19:35:46 +0200

    GET /accounts

--- src/main/java/com/sanctaultras/encryptedchat/user/account/AccountController.java
+++ src/main/java/com/sanctaultras/encryptedchat/user/account/AccountController.java
@@ -1,9 +1,12 @@
 package com.sanctaultras.encryptedchat.user.account;
 
+import com.sanctaultras.encryptedchat.user.UserRepository;
 import lombok.Data;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +19,13 @@ import javax.validation.constraints.Size;
 public class AccountController {
 
     private final AccountService accountService;
+    private final UserRepository userRepository;
+    private final UserDetailsServiceImpl userDetailsService;
 
-    public AccountController(AccountService accountService) {
+    public AccountController(AccountService accountService, UserRepository userRepository, UserDetailsServiceImpl userDetailsService) {
         this.accountService = accountService;
+        this.userRepository = userRepository;
+        this.userDetailsService = userDetailsService;
     }
 
     @PostMapping("/account/password")
@@ -28,6 +35,13 @@ public class AccountController {
         accountService.changePassword(user.getId(),changePasswordForm.getOldPassword(),changePasswordForm.getNewPassword());
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/accounts")
+    public ResponseEntity<?> getUsers(Pageable pageable) {
+        return ResponseEntity.ok(userRepository.findAll(pageable).map(userDetailsService::createUserDetails));
+    }
+
+
 }
 @Data
 class ChangePasswordForm {
