a3f794825106983b4e11d7548a0ed1734e0b524c
SanctaCC <mraxeo@gmail.com>
2018-08-28 19:20:41 +0200

    logout cors

--- src/main/java/com/sanctaultras/encryptedchat/user/UserRepository.java
+++ src/main/java/com/sanctaultras/encryptedchat/user/UserRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
-import org.springframework.lang.NonNull;
-import org.springframework.lang.NonNullApi;
 
 import java.util.Optional;
 
